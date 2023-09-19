dad5d323efcf92a4781653ec05066cca025f651b
Raphael <raphael.maringer@student.htldornbirn.at>
2023-09-19 09:20:21 +0200

    finished Java_IDE_Intro.java

---
+++ src/at/ram/units/Java_IDE_Intro.java
@@ -0,0 +1,20 @@
+package at.ram.units;
+
+public class Java_IDE_Intro {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        byte b = 7;
+        short s = 88;
+        long l = 999;
+        int x = 7;
+        double d = 34.3;
+        float f = 4.4f;
+        boolean isValid = true;
+        String text = "Hallo";
+
+        System.out.print("X:" + x + text);
+        System.out.println("X:" + x + text);
+        System.out.println("X:" + x + text);
+    }
+}
