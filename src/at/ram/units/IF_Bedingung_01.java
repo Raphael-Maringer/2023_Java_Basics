fb81af24053e93e601883b83258fcee35e49254c
Raphael <raphael.maringer@student.htldornbirn.at>
2023-09-22 09:17:35 +0200

    finished Java_IDE_Intro.java

--- src/at/ram/units/IF_Bedingung_01.java
+++ src/at/ram/units/IF_Bedingung_01.java
@@ -16,15 +16,4 @@ public class IF_Bedingung_01 {
             } else if (randomNumber > 50) {
                 System.out.println("Large");}
         }
-
-        //><
-
-        // Erstelle eine Zufallszahl zwischen 0 und 100
-
-        // Gib die Zufallszahl aus
-
-        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
-        // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
-// Wenn die Zahl größer als 50 ist gib aus "Large"
-
 }
