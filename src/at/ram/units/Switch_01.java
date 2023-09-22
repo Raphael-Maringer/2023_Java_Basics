f40ac7b714cd025af782d0c1c9c63e28e5a5714a
Raphael <raphael.maringer@student.htldornbirn.at>
2023-09-22 09:25:54 +0200

    finished Switch_01

---
+++ src/at/ram/units/Switch_01.java
@@ -0,0 +1,45 @@
+package at.ram.units;
+
+import java.util.Random;
+
+public class Switch_01 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(5,11);
+
+        switch (randomNumber){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            case 10:
+                System.out.println("Ten");
+                break;
+        }
+
+    }
+}
