696fbdcdb5108bf0d111a88667a217faffd3c63e
Raphael <raphael.maringer@student.htldornbirn.at>
2023-09-26 09:12:26 +0200

    finished While_01

---
+++ src/at/ram/units/While_01.java
@@ -0,0 +1,23 @@
+package at.ram.units;
+
+import java.util.Random;
+
+public class While_01 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        boolean isFinished = false;
+        int sum = 0;
+
+        while (!isFinished) {
+            int randomValue = random.nextInt(10,31);
+            sum += randomValue;
+
+            if ((randomValue==15)||(randomValue==25)) {
+                isFinished = true;
+            }
+
+        }
+
+        System.out.println(sum);
+    }
+}
