409699c713cf5f8c4f1e6491bc6fc1ebddb5aea1
Raphael <raphael.maringer@student.htldornbirn.at>
2023-10-11 09:13:49 +0200

    finished DiceGame

--- src/at/ram/units/DiceGame.java
+++ src/at/ram/units/DiceGame.java
@@ -3,49 +3,96 @@ package at.ram.units;
 import java.util.Scanner;
 
 import java.util.Random;
+
 public class DiceGame {
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+    static int playerScore = 0;
+    static int computerScore = 0;
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int randomNumber = random.nextInt(1,3);
-        boolean IsFinished = false;
+        System.out.println("Willkommen beim Würfelspiel!");
 
-        while(!IsFinished){
+        int randomNumber = random.nextInt(1, 3);
+        boolean isFinished = false;
+
+        while (!isFinished) {
             menu();
             System.out.println("Eingabe: ");
             int selection = scanner.nextInt();
 
-            switch (selection){
-                case 1: play();
-                        break;
+            switch (selection) {
+                case 1:
+                    play();
+                    break;
+                case 2:
+                    displayScore();
+                    break;
+                case 3:
+                    isFinished = true;
+                    break;
 
                 default:
-                    System.out.println("Bitte geben Sie eine Zahl zwischen 1 und 3 ein!");
+                    System.out.println("Bitte gib eine Zahl zwischen 1 und 3 ein!");
             }
         }
 
 
     }
 
-    public static void menu(){
-        System.out.println("Willkommen beim Würfelspiel!");
+    public static void menu() {
         System.out.println("1. Spiel starten");
         System.out.println("2. Punktestand anzeigen");
         System.out.println("3. Spiel beenden");
     }
 
-    public static void play(){
+    public static void updateScore(int playerRoll, int computerRoll) {
+        if (playerRoll > computerRoll) {
+            playerScore++;
+            System.out.println("Du gewinnst!");
+        } else if (computerRoll > playerRoll) {
+            computerScore++;
+            System.out.println("Der Computer gewinnt!");
+        } else {
+            System.out.println("Unentschieden!");
+        }
+        System.out.println("Drücke Enter, um fortzufahren!");
+    }
+
+    public static void displayScore() {
+        System.out.println("Punktestand:");
+        System.out.println("Spieler: " + playerScore);
+        System.out.println("Computer: " + computerScore);
+    }
+
+    public static void play() {
         int playerScore = 0;
         int computerScore = 0;
 
-        for (int i = 0; i <= 6 ; i++) {
+        for (int i = 1; i <= 6; i++) {
             System.out.println(i + ". Runde:");
-            System.out.println("Du bist am Zug! Drücke Enter, um zu würfeln!");
-            int playerRoll = randomNumber;
-            int computerRoll = randomNumber;
-        }
+            System.out.println("Du bist am Zug! Drücke 1, um zu würfeln, oder drücke 2, um den Punktestand anzuzeigen!");
+            int selection = scanner.nextInt();
+            scanner.nextLine();
 
+            if (selection == 1) {
+
+                int playerRoll = random.nextInt(1, 6);
+                System.out.println("Du hast eine " + playerRoll + " gewürfelt!");
+
+                int computerRoll = random.nextInt(1, 6);
+                System.out.println("Der Computer hat eine " + computerRoll + " gewürfelt!");
+
+                updateScore(playerRoll, computerRoll);
+                scanner.nextLine();
+            } else if (selection == 2) {
+                displayScore();
+            } else {
+                System.out.println("Bitte geben Sie 1 oder 2 ein!");
+            }
+
+        }
 
     }
 }
