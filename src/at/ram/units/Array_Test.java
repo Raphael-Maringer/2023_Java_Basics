74cc9192e512f3d1931e2708defc3c2ded2d98f4
Raphael <raphael.maringer@student.htldornbirn.at>
2023-10-11 09:32:54 +0200

    started Array_Test

---
+++ src/at/ram/units/Array_Test.java
@@ -0,0 +1,13 @@
+package at.ram.units;
+
+public class Array_Test {
+    public static void main(String[] args) {
+        int[] throwsArray = new int[6];
+        System.out.println(throwsArray[4]);
+
+        for (int i = 0; i < throwsArray.length; i++) {
+            System.out.println(throwsArray[i]);
+
+        }
+    }
+}
