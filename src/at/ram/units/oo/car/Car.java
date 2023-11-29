56184993a22fa198602b7e149127b710f77823dd
Raphael <raphael.maringer@student.htldornbirn.at>
2023-11-29 09:34:44 +0100

    finsihed oo.car (exercise 3)

--- src/at/ram/units/oo/car/Car.java
+++ src/at/ram/units/oo/car/Car.java
@@ -6,9 +6,32 @@ public class Car {
     String brand;
     String serialNumber;
     private String color;
+    int honkAmount;
+    int fuelVolume;
 
     public void drive(){
         this.fuelAmount = this.fuelAmount - this.fuelConsumption;
-        System.out.println("I am driving");
+        System.out.println("I am driving with " + fuelAmount + " l fuel remaining");
+    }
+
+    public void brake(){
+        System.out.println("Ich bremse");
+    }
+
+    public void turboboost(){
+        double fuelPercentage = fuelAmount / (double)fuelVolume * 100.0;
+        if (fuelPercentage > 10){
+            System.out.println("SuperBoostMode");
+        } else System.out.println("Not enough fuel to go to Superboost");
+    }
+    public void honk (int amountOfRepetitions){
+        for (int i = 0; i < amountOfRepetitions; i++) {
+            System.out.println("Tuuuuut");
+        }
+    }
+
+    public void getRemainingRange(){
+        double remainingRange = (double) fuelAmount / fuelConsumption;
+        System.out.println("The car has a remaining range of " + remainingRange + " km");
     }
 }
--- src/at/ram/units/oo/car/Main.java
+++ src/at/ram/units/oo/car/Main.java
@@ -9,12 +9,16 @@ public class Main {
         c1.fuelAmount = 70;
         c1.brand = "Audi";
         c1.serialNumber = "A1234";
+        c1.honkAmount = 3;
+        c1.fuelVolume = 80;
 
         Car c2 = new Car();
         c2.fuelConsumption = 6;
         c2.fuelAmount = 20;
         c2.brand = "Mercedes";
         c2.serialNumber = "M1234";
+        c2.honkAmount = 1;
+        c2.fuelVolume = 280;
 
         System.out.println(c1.serialNumber);
         System.out.println(c2.serialNumber);
@@ -24,6 +28,22 @@ public class Main {
         System.out.println(c1.fuelAmount);
         c1.drive();
         System.out.println(c1.fuelAmount);
+
+        c1.brake();
+
+        c1.turboboost();
+        c2.turboboost();
+        c1.honk(c1.honkAmount);
+
+        System.out.println("-------------------");
+
+        c1.getRemainingRange();
+
+        c1.drive();
+
+        c1.getRemainingRange();
+
+
     }
 
 }
