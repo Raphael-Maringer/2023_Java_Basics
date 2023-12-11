eea29a5bece47b8fcb2b5608d08207ea9312177e
Raphael <raphael.maringer@student.htldornbirn.at>
2023-12-11 14:44:32 +0100

    modified oo.car (4.2. getter & setter)

--- src/at/ram/units/oo/car/Car.java
+++ src/at/ram/units/oo/car/Car.java
@@ -1,49 +1,93 @@
 package at.ram.units.oo.car;
 
 public class Car {
-    /*int fuelConsumption;
-    public int fuelAmount;
-    String brand;
-    String serialNumber;
-    private String color;
-    int honkAmount;
-    int fuelVolume;
-    private double fuelConsumption;
+    private int fuelConsumption;
+    private int fuelAmount;
+    private String brand;
     private String serialNumber;
-    private double fuelAmount;
-    private int honAmount;
-    private String color;   */
+    private String color;
+    private int honkAmount;
+    private int fuelVolume;
 
-    public void drive(){
+    public Car(int fuelConsumption, String brand, String serialNumber) {
+        this.fuelConsumption = fuelConsumption;
+        this.brand = brand;
+        this.serialNumber = serialNumber;
+    }
+
+    public void drive() {
         this.fuelAmount = this.fuelAmount - this.fuelConsumption;
         System.out.println("I am driving with " + fuelAmount + " l fuel remaining");
     }
 
-    public void brake(){
+    public void brake() {
         System.out.println("Ich bremse");
     }
 
-    public void turboboost(){
-        double fuelPercentage = fuelAmount / (double)fuelVolume * 100.0;
-        if (fuelPercentage > 10){
+    public void turboboost() {
+        double fuelPercentage = fuelAmount / (double) fuelVolume * 100.0;
+        if (fuelPercentage > 10) {
             System.out.println("SuperBoostMode");
         } else System.out.println("Not enough fuel to go to Superboost");
     }
-    public void honk (int amountOfRepetitions){
+
+    public void honk(int amountOfRepetitions) {
         for (int i = 0; i < amountOfRepetitions; i++) {
             System.out.println("Tuuuuut");
         }
     }
 
-    public void getRemainingRange(){
+    public void getRemainingRange() {
         double remainingRange = (double) fuelAmount / fuelConsumption;
         System.out.println("The car has a remaining range of " + remainingRange + " km");
     }
 
-    public void setColor(String color) {
-        this.color = color;
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
-    public String getColor() {
-        return color;
+
+    public void setFuelAmount(int fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void setFuelVolume(int fuelVolume) {
+        this.fuelVolume = fuelVolume;
+    }
+
+    public void setHonkAmount(int honkAmount) {
+        this.honkAmount = honkAmount;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getFuelAmount() {
+        return fuelAmount;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public int getFuelVolume() {
+        return fuelVolume;
+    }
+
+    public int getHonkAmount() {
+        return honkAmount;
+    }
+
+    public String SerialNumber() {
+        return serialNumber;
     }
 }
--- src/at/ram/units/oo/car/Main.java
+++ src/at/ram/units/oo/car/Main.java
@@ -1,25 +1,32 @@
 package at.ram.units.oo.car;
+
 import at.ram.units.oo.car.Car;
 
 public class Main {
     public static void main(String[] args) {
 
         Car c1 = new Car();
-        c1.fuelConsumption = 7;
-        c1.fuelAmount = 70;
-        c1.brand = "Audi";
-        c1.serialNumber = "A1234";
-        c1.honkAmount = 3;
-        c1.fuelVolume = 80;
+        c1.setBrand("Audi");
+        c1.setFuelAmount(70);
+        c1.setFuelConsumption(30);
+        c1.setSerialNumber("A1234");
+        c1.setHonkAmount(3);
+        c1.setFuelVolume(70);
 
-        Car c2 = new Car();
+/*        Car c2 = new Car();
         c2.fuelConsumption = 6;
         c2.fuelAmount = 20;
         c2.brand = "Mercedes";
         c2.serialNumber = "M1234";
         c2.honkAmount = 1;
         c2.fuelVolume = 280;
+*/
+        Car c3 = new Car(17, "Opel", "1234O");
 
+
+        System.out.println(c1.getFuelAmount());
+
+        /*
         System.out.println(c1.serialNumber);
         System.out.println(c2.serialNumber);
 
@@ -42,7 +49,7 @@ public class Main {
         c1.drive();
 
         c1.getRemainingRange();
-
+    */
 
     }
 
