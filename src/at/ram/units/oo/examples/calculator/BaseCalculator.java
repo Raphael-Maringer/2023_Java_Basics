895c807544394ff94a5bcf27d8ad22b87e2c54ed
Raphael <raphael.maringer@student.htldornbirn.at>
2024-02-26 14:49:03 +0100

    14-calculator: started exercise (baseCalculator)

---
+++ src/at/ram/units/oo/examples/calculator/BaseCalculator.java
@@ -0,0 +1,23 @@
+package at.ram.units.oo.examples.calculator;
+
+import java.util.Scanner;
+
+public class BaseCalculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Geben Sie die erste Zahl ein: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Geben Sie die zweite Zahl ein: ");
+        int num2 = scanner.nextInt();
+        System.out.println(num1);
+
+        System.out.println("Wählen Sie die Rechenoperation (1 - 4):");
+        System.out.println("1 - Addition");
+        System.out.println("2 - Subtraktion");
+        System.out.println("3 - Multiplikation");
+        System.out.println("4 - Division");
+    }
+
+}
